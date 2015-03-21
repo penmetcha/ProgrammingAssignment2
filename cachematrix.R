c57f04e023c950c87895c89ab5a942401f7ab5a7
penmetcha <pvdvarma@hotmail.com>
2015-03-20 19:06:43 -0500

    Adding new file HelloWorld.md

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Program generates inverse of any matrix
+## chacheSolve generates inverse matrix and MakeCahe function stores values in cache for reuse
+## Cachematrix acts like a cnstuctor
 
 makeCacheMatrix <- function(x = matrix()) {
+	m <- NULL
+    set <- function(y) {
+       
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() {
+                       
+                       x
+                        }
+    setsolve <- function(solve) m <<- solve
+    getsolve <- function() m
+    list(set = set, get = get,
+         setsolve = setsolve,
+         getsolve = getsolve)
 
 }
 
 
-## Write a short comment describing this function
+## This function is to inverse a matrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    m <- x$getsolve()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+        
+    }
+    print("c")
+    data <- x$get()
+    print(data)
+    m <- solve(data, ...)
+    x$setsolve(m)
+    m
 }
